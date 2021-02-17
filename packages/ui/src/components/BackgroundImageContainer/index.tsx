5709358d87ca2c4d10a2cad328b833dd35bc8674
Anthony Martin <anthony.martin@honestfoodcompany.de>
2021-02-18 00:09:27 +0100

    remove blur

--- packages/ui/src/components/BackgroundImageContainer/index.tsx
+++ packages/ui/src/components/BackgroundImageContainer/index.tsx
@@ -18,7 +18,6 @@ export const BackgroundImageContainer = styled.div<{ imgSrc: string }>`
   background-position: center center;
   background: rgb(131,58,180);
   background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
-  filter: blur(4px);
   opacity: 1;
 
 `;
