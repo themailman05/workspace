f14da9f7e96e0b46412d7930809b7678b3b9d748
Anthony Martin <anthony@replaycreative.com>
2021-04-02 20:49:15 +0200

    adds defaults, registration  and initialization

--- packages/contracts/contracts/GrantElections.sol
+++ packages/contracts/contracts/GrantElections.sol
@@ -1,7 +1,8 @@
-pragma solidity >=0.7.0 <0.8.0;
+pragma solidity >=0.7.0 <= 0.8.2;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "./IStaking.sol";
+import "./IBeneficiaryRegistry.sol";
 
 contract GrantElections {
     using SafeMath for uint256;
@@ -15,28 +16,94 @@ contract GrantElections {
     enum ElectionTerm {Monthly, Quarterly, Yearly}
     enum ElectionState {Registration, Voting, Closed}
 
+    uint8 constant ONE_DAY = 86400; // seconds in 1 day
+
     struct Election {
         Vote[] votes;
+        mapping(address => bool) registeredBeneficiaries;
         ElectionTerm electionTerm;
         ElectionState electionState;
         ElectionConfiguration electionConfiguration;
+        uint256 startTime;
     }
 
     struct ElectionConfiguration {
         uint8 ranking;
         uint8 awardees;
+        uint256 cooldownPeriod;
     }
 
     // mapping of election terms and beneficiary total votes
+    Election[3] private elections;
     mapping(ElectionTerm => mapping(address => uint256)) beneficiaryVotes;
-    mapping(ElectionTerm => Election) elections;
     mapping(ElectionTerm => ElectionConfiguration) electionConfigurations;
     mapping(ElectionTerm => mapping(uint8 => address)) electionRanking;
 
-    IStaking staking;
+    ElectionConfiguration[3] private electionDefaults;  
 
-    constructor(IStaking _staking) {
+    IStaking staking;
+    IBeneficiaryRegistry beneficiaryRegistry;
+
+    event BeneficiaryRegistered(address _beneficiary, ElectionTerm _term);
+
+    constructor(IStaking _staking, IBeneficiaryRegistry _beneficiaryRegistry) {
         staking = _staking;
+        beneficiaryRegistry = _beneficiaryRegistry;
+
+        _setDefaults();
+    }
+
+    function _setDefaults() internal {
+        ElectionConfiguration storage monthlyDefaults = electionDefaults[uint(ElectionTerm.Monthly)];
+        monthlyDefaults.awardees = 1;
+        monthlyDefaults.ranking = 3;
+        monthlyDefaults.cooldownPeriod = 21 * ONE_DAY; 
+
+        ElectionConfiguration storage quarterlyDefaults = electionDefaults[uint(ElectionTerm.Quarterly)];
+        quarterlyDefaults.awardees = 2;
+        quarterlyDefaults.ranking = 5;
+        quarterlyDefaults.cooldownPeriod = 83 * ONE_DAY; 
+
+        ElectionConfiguration storage yearlyDefaults = electionDefaults[uint(ElectionTerm.Yearly)];
+        yearlyDefaults.awardees = 3;
+        yearlyDefaults.ranking = 7;
+        yearlyDefaults.cooldownPeriod = 358 * ONE_DAY;
+    }
+
+    // todo: mint POP for caller to incentivize calling function
+    function initialize(ElectionTerm _grantTerm) public {
+        uint8 _term = uint8(_grantTerm);
+        Election _election = elections[_term];
+        require(!_election || _election.electionState == ElectionState.Closed, "election can't be started yet");
+
+        if (_election && _election.electionState == ElectionState.Closed) {
+            require(
+                _election.electionConfiguration.cooldownPeriod >= block.timestamp.sub(_election.startTime), 
+                "can't start new election, not enough time elapsed since last election"
+            );
+        }
+
+        delete election[_term];
+
+        Election storage e = election[_term];
+        e.electionConfiguration = electionDefaults[_term];
+        e.electionState = ElectionState.Registration;
+        e.electionTerm = _term;
+        e.startTime = block.timestamp;
+    }
+
+    // todo: use POP for bond
+    function registerForElection(address memory _beneficiary, ElectionTerm _term) public {
+        require(elections[_term].electionState == ElectionState.Registration, "election not open for registration");
+        require(beneficiaryRegistry.beneficiaryExists(_beneficiary), "address is not eligible for registration");
+
+        elections[_term].registeredBeneficiaries[_beneficiary] = true;
+
+        emit BeneficiaryRegistered(_beneficiary, _term);
+    }
+
+    function _isRegisteredBeneficiary(address memory _beneficiary, ElectionTerm _term) view public returns (bool) {
+        return elections[_term].registeredBeneficiaries[_beneficiary];
     }
 
     function vote(address[] memory _beneficiaries, uint8[] memory _voiceCredits, ElectionTerm _electionTerm) public {
@@ -47,7 +114,12 @@ contract GrantElections {
         uint256 _usedVoiceCredits = 0;
         uint256 _stakedVoiceCredits = staking.getVoiceCredits(msg.sender);
 
+        require(_stakedVoiceCredits > 0, "must have voice credits from staking");
+
         for (uint8 i = 0; i < _beneficiaries.length; i++) {
+
+            require(_isRegisteredBeneficiary(_beneficiaries[i], _electionTerm), "ineligible beneficiary");
+
             _usedVoiceCredits = _usedVoiceCredits.add(_voiceCredits[i]);
             uint256 _sqredVoiceCredits = sqrt(_voiceCredits[i]);
 
@@ -60,7 +132,7 @@ contract GrantElections {
             elections[_electionTerm].votes.push(_vote);
             beneficiaryVotes[_electionTerm][_beneficiaries[i]] = beneficiaryVotes[_electionTerm][_beneficiaries[i]].add(_sqredVoiceCredits);
         }
-        require(_usedVoiceCredits <= _stakedVoiceCredits, "Insuficcient voice credits");
+        require(_usedVoiceCredits <= _stakedVoiceCredits, "Insufficient voice credits");
     }
 
     function _recalculateRanking(ElectionTerm _electionTerm, address _beneficiary, uint256 weight) internal {
