6bd7d28c523cbabd02c6e51676c0d5c24692f264
Emilio Tagua <miloops@gmail.com>
2021-04-04 21:48:05 +0200

    Fix tests

--- packages/contracts/test/GrantElections.test.js
+++ packages/contracts/test/GrantElections.test.js
@@ -14,8 +14,17 @@ describe('GrantElections', function () {
     });
 
     beforeEach(async function () {
-        const Staking = await ethers.getContractFactory('GrantElections');
-        this.contract = await Staking.deploy(this.mockPop.address, this.mockPop.address);
+        Staking = await ethers.getContractFactory("Staking");
+        this.mockStaking = await waffle.deployMockContract(owner, Staking.interface.format());
+
+        BeneficiaryRegistry = await ethers.getContractFactory("BeneficiaryRegistry");
+        this.mockBeneficiaryRegistry = await waffle.deployMockContract(owner, BeneficiaryRegistry.interface.format());
+
+        const GrantElections = await ethers.getContractFactory('GrantElections');
+        this.contract = await GrantElections.deploy(
+            this.mockStaking.address,
+            this.mockBeneficiaryRegistry.address
+           );
         await this.contract.deployed();
     });
 
@@ -33,9 +42,20 @@ describe('GrantElections', function () {
         });
 
         it("should require election open for voting", async function () {
+            await this.contract.initialize(0);
             await expect(
                 this.contract.vote([beneficiary.address], [1], 0)
             ).to.be.revertedWith("Election not open for voting");
+        });
+
+        it("should require staked voice credits", async function () {
+            await this.contract.initialize(0);
+            ethers.provider.send("evm_increaseTime", [7 * 86400]);
+            ethers.provider.send("evm_mine");
+            await this.mockStaking.mock.getVoiceCredits.returns(0);
+            await expect(
+                this.contract.vote([beneficiary.address], [1], 0)
+            ).to.be.revertedWith("must have voice credits from staking");
         });
     });
 });
