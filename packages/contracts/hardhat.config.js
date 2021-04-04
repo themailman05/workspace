d3fba4dc382eb606b39388fea32df91234649cf7
Anthony Martin <anthony@replaycreative.com>
2021-04-04 14:45:40 +0200

    adds tasks to mint POP and to check balance of pop (hardhat POP:mint / hardhat POP:balanceOf)

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+const { parseEther } = require("ethers/lib/utils");
+const mockERC20ABI = require('./artifacts/contracts/mocks/MockERC20.sol/MockERC20.json').abi;
 
 task("accounts", "Prints the list of accounts", async () => {
   const accounts = await ethers.getSigners();
@@ -7,6 +9,30 @@ task("accounts", "Prints the list of accounts", async () => {
     console.log(account.address);
   }
 });
+
+task("POP:mint", "mint amount for recipient",)
+  .addParam("contractaddress", "mock POP contract address")
+  .addParam("recipient", "address to receive POP")
+  .addParam("amount", "amount to receive")
+  .setAction(async (args, hre) => {
+    const [signer] = await ethers.getSigners();
+    const { contractaddress, recipient, amount } = args;
+    const mockPOP = new ethers.Contract(contractaddress, mockERC20ABI, signer);
+    const result = await mockPOP.mint(recipient, amount);
+    console.log("Done: ", result);
+});
+
+task("POP:balanceOf", "get balance of POP for address",)
+  .addParam("contractaddress", "mock POP contract address")
+  .addParam("address", "amount to receive")
+  .setAction(async (args, hre) => {
+    const [signer] = await ethers.getSigners();
+    const { contractaddress, address } = args;
+    const mockPOP = new ethers.Contract(contractaddress, mockERC20ABI, signer);
+    const result = await mockPOP.balanceOf(address);
+    console.log("Result: ", result.toNumber());
+});
+
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
