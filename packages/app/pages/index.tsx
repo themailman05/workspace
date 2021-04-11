cb97e913a20135113106275df6d1268e754b4855
Anthony Martin <anthony@replaycreative.com>
2021-04-11 04:03:26 +0200

    adds netlify router hack

--- packages/app/pages/index.tsx
+++ packages/app/pages/index.tsx
@@ -4,7 +4,11 @@ import { LandingPage } from '../components/Grants/LandingPage';
 
 const IndexPage = () => {
   const router = useRouter();
-
+  useEffect(() => {
+    if (typeof window !== "undefined" && window.location.pathname !== "/") {
+      router.replace(window.location.pathname);
+    }
+  }, [router.pathname]);
 
   return (
     <LandingPage />
