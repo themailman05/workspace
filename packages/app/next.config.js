7d602f560348f8fed2cdd654ec58d1739c549415
RedVeil <leon.niesler@gmail.com>
2021-06-07 09:21:07 +0200

    compile fixes

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -1,11 +1,22 @@
 const { join } = require('path');
-require('dotenv').config();
+
+require('../utils/src/envLoader');
 
 const workspace = join(__dirname, '..');
 
 module.exports = {
+  //target: 'serverless',
   env: {
     RPC_URL: process.env.RPC_URL,
+    CHAIN_ID: process.env.CHAIN_ID,
+    ADDR_STAKING: process.env.ADDR_STAKING,
+    ADDR_POP: process.env.ADDR_POP,
+    ADDR_GRANT_REGISTRY: process.env.ADDR_GRANT_REGISTRY,
+    ADDR_BENEFICIARY_REGISTRY: process.env.ADDR_BENEFICIARY_REGISTRY,
+    ADDR_GRANT_ELECTION: process.env.ADDR_GRANT_ELECTION,
+    ADDR_PRIVATE_SALE: process.env.ADDR_PRIVATE_SALE,
+    ADDR_USDC: process.env.ADDR_USDC,
+    ADDR_TREASURY: process.env.ADDR_TREASURY,
   },
   poweredByHeader: false,
   webpack: (config, options) => {
