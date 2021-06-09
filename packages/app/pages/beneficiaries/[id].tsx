7cbabf54f258caa934523c9ce40eb2b63f76fba6
RedVeil <leon.niesler@gmail.com>
2021-06-09 10:32:17 +0200

    init

--- packages/app/pages/beneficiaries/[id].tsx
+++ packages/app/pages/beneficiaries/[id].tsx
@@ -5,30 +5,5 @@ import { useContext, useEffect, useState } from 'react';
 import { getIpfsHashFromBytes32 } from '@popcorn/utils/ipfsHashManipulation';
 
 export default function BeneficiaryPageWrapper(): JSX.Element {
-  const router = useRouter();
-  const { contracts } = useContext(ContractsContext);
-  const [benefeciary, setBeneficiary] = useState();
-
-  async function getBeneficiary() {
-    const ipfsHash = await contracts.beneficiary.getBeneficiary(
-      router.query.id as string,
-    );
-    console.log(ipfsHash)
-    const ipfsData = await fetch(
-      `${process.env.IPFS_URL}${getIpfsHashFromBytes32(ipfsHash)}`,
-    ).then((response) => response.json());
-
-    console.log(ipfsData)
-    setBeneficiary(ipfsData);
-  }
-
-  useEffect(() => {
-    if (contracts) {
-      getBeneficiary();
-    }
-  }, [contracts]);
-
-  console.log(benefeciary)
-
   return <BeneficiaryPage isProposal={false} />;
 }
