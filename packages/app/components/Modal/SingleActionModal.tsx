85d986f917cf468318edb8ecab574bfb21eedc6f
mefuru <mehul.mandania@gmail.com>
2021-06-21 12:40:45 +0100

    css warning clean up

--- packages/app/components/Modal/SingleActionModal.tsx
+++ packages/app/components/Modal/SingleActionModal.tsx
@@ -97,7 +97,7 @@ export const SingleActionModal: React.FC<SingleActionModalProps> = ({
           <div className="mt-5 sm:mt-6">
             <button
               type="button"
-              className="button button-primary w-full inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+              className="button button-primary w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
               onClick={() => onConfirm.onClick && onConfirm.onClick()}
             >
               {onConfirm.label}
