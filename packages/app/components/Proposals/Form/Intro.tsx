b275beddfaefdca4f3052976f89a9912cc96453c
mefuru <mehul.mandania@gmail.com>
2021-06-22 13:24:19 +0100

    update copy

--- packages/app/components/Proposals/Form/Intro.tsx
+++ packages/app/components/Proposals/Form/Intro.tsx
@@ -12,29 +12,46 @@ export default function Intro({
     visible && (
       <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
         <div className="sm:text-center lg:text-left">
-          <h1 className="text-4xl mb-8 tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
-            <span className="block xl:inline">
-              Beneficiary Nomination Proposal
-            </span>
-          </h1>
-          <p className="mt-3 mr-10 text-lg tracking-tight text-gray-500 sm:text-lg md:text-lg">
-            For an organization to become an eligible grant recipient, a
-            Beneficiary Nomination Proposal (BNP) must be raised and the
-            proposal must receive a majority of votes cast towards “Yes” with at
-            least 10% of the available supply of governance tokens voting “Yes”.
+          <h3 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+            Beneficiary Nomination Proposal Form
+          </h3>
+          <p className="mt-8 text-lg text-gray-500">
+            Thank you for making the effort to nominate an organization for
+            grant election eligibility.
           </p>
-          <p className="mt-3 mr-10 text-lg tracking-tight text-gray-500 sm:text-lg md:text-lg">
+          <p className="mt-8 text-lg text-gray-500">
+            Anyone may nominate an organization by completing this form. It is
+            also possible for an organization to nominate itself.
+          </p>
+          <p className="mt-8 text-lg text-gray-500">
+            After the form has been submitted, the organization will be subject
+            to a round of voting. If the organisation receives a majority of
+            votes cast towards “Yes” (with at least 10% of the available supply
+            of governance tokens voting “Yes”), it will become eligible for
+            grants via grant elections.
+          </p>
+          <p className="mt-8 text-lg text-gray-500">
             An organization wishing to apply for eligible beneficiary status may
             acquire the requisite number of POP tokens to raise a BNP (2000), or
             they may reach out to the Popcorn Foundation to seek a nomination at
             no cost.
           </p>
-          <p className="mt-3 mr-10 text-lg tracking-tight text-gray-500 sm:text-lg md:text-lg">
-            Supplementary application materials, such as mission statement,
-            proof of address ownership, photos, links to social media accounts,
-            and links to impact reports will be included as an IPFS content
-            hash.
-          </p>
+          <div className="mt-5 prose prose-indigo text-gray-500">
+            <p className="mt-8 text-lg text-gray-500">
+              You'll need the following things to submit a proposal:
+            </p>
+            <ul>
+              <li>A connected wallet with 2000 Popcorn governance tokens</li>
+              <li>Your nominee's name and mission statement</li>
+              <li>Ethereum address to which rewards will be transferred</li>
+              <li>Proof that the address belongs to the nominee</li>
+              <li>Impact reports and/or audited documents</li>
+              <li>
+                Supplmentary information such as photos and links to social
+                media accounts
+              </li>
+            </ul>
+          </div>
           <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
             {/* Check for partially completed form */}
             {formData.name !== '' ? (
