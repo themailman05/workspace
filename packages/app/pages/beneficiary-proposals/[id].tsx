eaf36bb887a181c581df5d2338a403c8f7045d2e
mefuru <mehul.mandania@gmail.com>
2021-06-24 20:47:41 +0200

    source proposalId from url rather than router props

--- packages/app/pages/beneficiary-proposals/[id].tsx
+++ packages/app/pages/beneficiary-proposals/[id].tsx
@@ -1,18 +1,19 @@
 import ProposalPage from 'components/Proposals/ProposalPage';
 import { ContractsContext } from 'context/Web3/contracts';
 import { Proposal } from 'interfaces/interfaces';
-import router from 'next/router';
 import { useContext, useEffect, useState } from 'react';
 import { getProposal } from 'utils/getProposals';
 
 export default function SingleProposalPage(): JSX.Element {
   const { contracts } = useContext(ContractsContext);
   const [proposal, setProposal] = useState<Proposal>();
+  const [proposalId, setProposalId] = useState<string>('');
+  useEffect(() => {
+    setProposalId(window.location.pathname.split('/').pop());
+  }, []);
   useEffect(() => {
     if (contracts) {
-      getProposal(contracts, router.query.id as string).then((res) =>
-        setProposal(res),
-      );
+      getProposal(contracts, proposalId).then((res) => setProposal(res));
     }
   }, [contracts]);
   return <ProposalPage {...proposal} />;
