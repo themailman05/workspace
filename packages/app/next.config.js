0967272caa9a6c77725a3bf03bd79c628c9074a7
mefuru <mehul.mandania@gmail.com>
2021-07-05 13:38:46 +0200

    add target serverless

--- packages/app/next.config.js
+++ packages/app/next.config.js
@@ -5,6 +5,7 @@ require('../utils/src/envLoader');
 const workspace = join(__dirname, '..');
 
 module.exports = {
+  target: 'serverless',
   distDir: 'dist/apps/react-docs/.next',
   env: {
     RPC_URL: process.env.RPC_URL,
