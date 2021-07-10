7fc9d273423368f5f704285d65e6a40fbd87f84a
mefuru <mehul.mandania@gmail.com>
2021-07-10 11:39:51 +0200

    Update image header

--- packages/app/components/CommonComponents/ImageHeader.tsx
+++ packages/app/components/CommonComponents/ImageHeader.tsx
@@ -6,33 +6,38 @@ export default function ImageHeader({
 }: {
   beneficiary: BeneficiaryApplication;
   title?: string;
-  }): JSX.Element {
+}): JSX.Element {
   return (
-    <div className="relative bg-indigo-800">
-      <div className="absolute inset-0">
+    <div>
+      <div>
         <img
-          className="w-full h-full object-cover"
+          className="h-32 w-full object-cover lg:h-48"
           src={`${process.env.IPFS_URL}${beneficiary?.files?.headerImage}`}
           alt=""
         />
-        <div
-          className="absolute inset-0 bg-indigo-800 mix-blend-multiply"
-          aria-hidden="true"
-        />
       </div>
-      <div className="relative max-w-7xl mx-auto py-24 px-4 sm:py-32 sm:px-6 lg:px-8">
-        <h1 className="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">
-          {title ? title : beneficiary.organizationName}
-        </h1>
-        <div
-          className="absolute -bottom-32 left-10 shadow-lg rounded-full h-60 w-60 border-black "
-          style={{
-            backgroundImage: `url(${process.env.IPFS_URL}${beneficiary?.files?.profileImage})`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-          }}
-        />
+      <div className="max-w-5xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="-mt-12 sm:-mt-16 sm:flex sm:items-end sm:space-x-5">
+          <div className="flex">
+            <img
+              className="h-24 w-24 rounded-full ring-4 ring-white sm:h-32 sm:w-32"
+              src={`${process.env.IPFS_URL}${beneficiary?.files?.profileImage}`}
+              alt=""
+            />
+          </div>
+          <div className="mt-6 sm:flex-1 sm:min-w-0 sm:flex sm:items-center sm:justify-end sm:space-x-6 sm:pb-1">
+            <div className="sm:hidden md:block mt-6 min-w-0 flex-1">
+              <h1 className="text-2xl font-bold text-gray-900 truncate">
+                {title ? title : beneficiary.organizationName}
+              </h1>
+            </div>
+          </div>
+        </div>
+        <div className="hidden sm:block md:hidden mt-6 min-w-0 flex-1">
+          <h1 className="text-2xl font-bold text-gray-900 truncate">
+            {title ? title : beneficiary.organizationName}
+          </h1>
+        </div>
       </div>
     </div>
   );
