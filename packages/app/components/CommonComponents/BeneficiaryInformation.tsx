c2b9d89850b5caf39f3e6e6e1b10322e1db26e38
mefuru <mehul.mandania@gmail.com>
2021-07-12 14:51:38 +0200

    add project name to Preview/Beneficiary Information

--- packages/app/components/CommonComponents/BeneficiaryInformation.tsx
+++ packages/app/components/CommonComponents/BeneficiaryInformation.tsx
@@ -15,6 +15,16 @@ export default function BeneficiaryInformation(
         </div>
         <div className="border-t border-gray-200 px-4 py-5 sm:p-0">
           <dl className="sm:divide-y sm:divide-gray-200">
+            {beneficiary?.projectName! == '' && (
+              <div className="bg-gray-50 py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt className="text-sm font-medium text-gray-500">
+                  Project Name
+                </dt>
+                <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  {beneficiary?.projectName}
+                </dd>
+              </div>
+            )}
             <div className="bg-gray-50 py-4 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt className="text-sm font-medium text-gray-500">
                 Mission Statement
