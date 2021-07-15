be759b87d0f0934061dc2963121447bfd7af3cb1
mefuru <mehul.mandania@gmail.com>
2021-07-15 14:46:03 +0200

    centre image description text boxes

--- packages/app/components/Proposals/Form/ProfileImage.tsx
+++ packages/app/components/Proposals/Form/ProfileImage.tsx
@@ -68,7 +68,7 @@ export default function ProfileImage({
           maxFileSizeMB={5}
         />
         <DisplayImages localState={formData?.files?.profileImage?.image} />
-        <div className="mt-8 w-80">
+        <div className="mx-auto mt-8 w-80">
           <p>Image Description</p>
           <ControlledTextInput
             inputValue={formData?.files?.profileImage?.description}
