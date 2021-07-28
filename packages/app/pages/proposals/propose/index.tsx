7bf2ea66c6f8877e702a4e30bfeab947918b3dda
mefuru <mehul.mandania@gmail.com>
2021-07-28 14:16:06 +0200

    stop steps oscillating between intro and other steps

--- packages/app/pages/proposals/propose/index.tsx
+++ packages/app/pages/proposals/propose/index.tsx
@@ -63,7 +63,7 @@ export const defaultFormData: BeneficiaryApplication = {
 const stepOrder: string[] = [
   'intro',
   'name',
-  'projectName',
+  'project-name',
   'beneficiary-address',
   'mission-statement',
   'proof-of-ownership',
@@ -79,7 +79,7 @@ const stepOrder: string[] = [
 ];
 
 export default function BeneficiaryProposal(): JSX.Element {
-  const [currentStep, setCurrentStep] = useState<number>(0);
+  const [currentStep, setCurrentStep] = useState<number>();
   const [formData, setFormData] =
     useState<BeneficiaryApplication>(defaultFormData);
   const router = useRouter();
@@ -89,17 +89,15 @@ export default function BeneficiaryProposal(): JSX.Element {
   }, []);
 
   useEffect(() => {
-    console.log(1);
     const stepName = router.query.step as string;
     const stepIndex = stepOrder.indexOf(stepName);
-    if (stepName && stepIndex !== -1) {
+    if (stepIndex !== undefined && stepIndex !== -1) {
       if (stepIndex !== undefined && stepIndex !== currentStep)
         setCurrentStep(stepIndex);
     }
   }, [router]);
 
   useEffect(() => {
-    console.log(2);
     const stepName = stepOrder[currentStep];
     router.push(`/proposals/propose/?step=${stepName}`, undefined, {
       shallow: true,
@@ -121,81 +119,85 @@ export default function BeneficiaryProposal(): JSX.Element {
   return (
     <div className="flex flex-col h-screen justify-between">
       <NavBar />
-      <Intro
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'intro'}
-      />
-      <Name
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'name'}
-      />
-      <ProjectName
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'projectName'}
-      />
-      <BeneficiaryAddress
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'beneficiary-address'}
-      />
-      <MissionStatement
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'mission-statement'}
-      />
-      <ProofOfOwnership
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'proof-of-ownership'}
-      />
-      <ProfileImage
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'profile-image'}
-      />
-      <HeaderImage
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'header-image'}
-      />
-      <AdditionalImages
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'additional-images'}
-      />
-      <Video
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'video'}
-      />
-      <ImpactReportsAudits
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'impact-reports-audits'}
-      />
-      <Website
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'website'}
-      />
-      <ContactEmail
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'contact-email'}
-      />
-      <SocialMedia
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'social-media'}
-      />
-      <Preview
-        form={[formData, setFormData]}
-        navigation={navigation}
-        visible={stepOrder[currentStep] === 'preview'}
-      />
+      {currentStep !== undefined && (
+        <div className="mt-10 mx-auto max-w-7xl">
+          <Intro
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'intro'}
+          />
+          <Name
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'name'}
+          />
+          <ProjectName
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'project-name'}
+          />
+          <BeneficiaryAddress
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'beneficiary-address'}
+          />
+          <MissionStatement
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'mission-statement'}
+          />
+          <ProofOfOwnership
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'proof-of-ownership'}
+          />
+          <ProfileImage
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'profile-image'}
+          />
+          <HeaderImage
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'header-image'}
+          />
+          <AdditionalImages
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'additional-images'}
+          />
+          <Video
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'video'}
+          />
+          <ImpactReportsAudits
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'impact-reports-audits'}
+          />
+          <Website
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'website'}
+          />
+          <ContactEmail
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'contact-email'}
+          />
+          <SocialMedia
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'social-media'}
+          />
+          <Preview
+            form={[formData, setFormData]}
+            navigation={navigation}
+            visible={stepOrder[currentStep] === 'preview'}
+          />
+        </div>
+      )}
       <NavigationButtons navigation={navigation} />
       <Toaster position="top-right" />
     </div>
