5b4f4c5ff447b164ee0054153a04ffb4d0977105
RedVeil <leon.niesler@gmail.com>
2021-07-28 11:52:48 +0200

    wip

--- packages/contracts/contracts/RewardsEscrowV2.sol
+++ packages/contracts/contracts/RewardsEscrowV2.sol
@@ -1,185 +1,210 @@
 pragma solidity >=0.7.0 <0.8.0;
 
-import "./IStaking.sol";
-import "./IRewardsEscrow.sol";
-import "./Owned.sol";
-import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/math/Math.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import "./Governed.sol";
 
-/**
- * @title Popcorn Rewards Escrow
- * @notice Vests Token from the Staking contract.
- */
-contract RewardsEscrow is IRewardsEscrow, Owned, ReentrancyGuard {
+contract ParticipationReward is Governed, ReentrancyGuard {
   using SafeMath for uint256;
   using SafeERC20 for IERC20;
 
   /* ========== STATE VARIABLES ========== */
-
   struct Escrow {
     uint256 start;
     uint256 end;
-    uint256 amount;
+    uint256 balance;
   }
 
   IERC20 public immutable POP;
-  IStaking public Staking;
-  mapping(address => Escrow[2]) public escrowedBalances;
-  mapping(address => uint256) public vested;
-  uint256 public escrowDuration = 90 days;
-  uint256 public vestingCliff = 90 days;
+  mapping(bytes32 => Escrow) public escrows;
+  mapping(address => bytes32[]) public escrowIds;
 
   /* ========== EVENTS ========== */
-
-  event Claimed(address _address, uint256 claimable);
-  event Locked(address _address, uint256 claimable);
-  event StakingChanged(IStaking _staking);
-  event EscrowDurationChanged(uint256 _escrowDuration);
-  event VestingCliffChanged(uint256 _vestingCliff);
+  event RewardBudgetChanged(uint256 amount);
+  event VaultInitialized(bytes32 vaultId);
+  event VaultOpened(bytes32 vaultId);
+  event VaultClosed(bytes32 vaultId);
+  event RewardClaimed(bytes32 vaultId, address account_, uint256 amount);
+  event RewardsClaimed(address account_, uint256 amount);
+  event SharesAdded(bytes32 vaultId_, address account_, uint256 shares_);
+  event RewardBalanceIncreased(address account, uint256 amount);
 
   /* ========== CONSTRUCTOR ========== */
 
-  constructor(IERC20 _pop) Owned(msg.sender) {
+  constructor(IERC20 _pop, address _governance) Governed(_governance) {
     POP = _pop;
   }
 
   /* ========== VIEWS ========== */
 
-  function getLocked(address _address) external view returns (uint256) {
-    return escrowedBalances[_address][0].amount;
+  /**
+   * @notice Checks if a beneficiary has a claim in the specified vault
+   * @param vaultId_ Bytes32
+   * @param beneficiary_ address of the beneficiary
+   */
+  function hasClaim(bytes32 vaultId_, address beneficiary_)
+    public
+    view
+    vaultExists(vaultId_)
+    returns (bool)
+  {
+    return
+      vaults[vaultId_].shareBalances[beneficiary_] > 0 &&
+      vaults[vaultId_].claimed[beneficiary_] == false;
   }
 
-  function getVested(address _address) public view returns (uint256) {
-    uint256 _now = block.timestamp;
-    uint256 locked = escrowedBalances[_address][0].amount;
-    uint256 start = escrowedBalances[_address][0].start;
-    uint256 end = escrowedBalances[_address][0].end;
-    if (_now < start) {
-      return 0;
-    }
-    if (start == 0 || end == 0) {
-      return 0;
-    }
-    return Math.min((locked.mul(_now.sub(start))).div(end.sub(start)), locked);
+  /**
+   * @notice Returns the vault status
+   * @param vaultId_ Bytes32
+   */
+  function getVaultStatus(bytes32 vaultId_)
+    external
+    view
+    returns (VaultStatus)
+  {
+    return vaults[vaultId_].status;
+  }
+
+  /**
+   * @notice Returns all vaultIds which an account has/had claims in
+   * @param account address
+   */
+  function getUserVaults(address account)
+    external
+    view
+    returns (bytes32[] memory)
+  {
+    return userVaults[account];
   }
 
   /* ========== MUTATIVE FUNCTIONS ========== */
 
-  function lock(address _address, uint256 _amount)
-    external
-    override
-    nonReentrant
-    updateVested(_address)
-  {
-    require(msg.sender == address(Staking), "you cant call this function");
-    require(_amount > 0, "amount must be greater than 0");
-    require(POP.balanceOf(msg.sender) >= _amount, "insufficient balance");
-
-    POP.safeTransferFrom(msg.sender, address(this), _amount);
-
-    _lock(_address, _amount);
-  }
-
-  function claim() public nonReentrant updateVested(msg.sender) {
-    _claimFor(msg.sender);
-  }
-
-  /* ========== RESTRICTED FUNCTIONS ========== */
-
-  function _lock(address _address, uint256 _amount) internal {
+  /**
+   * @notice Initializes a vault for voting claims
+   * @param vaultId_ Bytes32
+   * @param endTime_ Unix timestamp in seconds after which a vault can be closed
+   * @dev There must be enough funds in this contract to support opening another vault
+   */
+  function lock(address account_, uint256 amount_) external {
     uint256 _now = block.timestamp;
-
-    if (
-      _now > escrowedBalances[_address][0].end &&
-      escrowedBalances[_address][1].end != 0
-    ) {
-      escrowedBalances[_address][0] = escrowedBalances[_address][1];
-      delete escrowedBalances[_address][1];
-    }
-
     uint256 _start = _now.add(vestingCliff);
     uint256 _end = _start.add(escrowDuration);
 
-    if (
-      _start > escrowedBalances[_address][0].end &&
-      escrowedBalances[_address][0].end != 0
-    ) {
-      if (_end < escrowedBalances[_address][1].end) {
-        _end = escrowedBalances[_address][1].end;
-      }
-      if (
-        _start > escrowedBalances[_address][1].start &&
-        escrowedBalances[_address][0].start != 0
-      ) {
-        _start = escrowedBalances[_address][1].start;
-      }
-      escrowedBalances[_address][1] = Escrow({
-        start: _start,
-        end: _end,
-        amount: _amount.add(escrowedBalances[_address][1].amount)
-      });
-      return;
-    }
+    bytes32 id = keccak256(abi.encodePacked(account_, amount_, _now));
 
-    if (
-      _end > escrowedBalances[_address][1].start &&
-      escrowedBalances[_address][1].start != 0
-    ) {
-      _amount = _amount.add(escrowedBalances[_address][1].amount);
-      delete escrowedBalances[_address][1];
-    }
-
-    if (_start < escrowedBalances[_address][0].end) {
-      _start = escrowedBalances[_address][0].start;
-    }
-
-    escrowedBalances[_address][0] = Escrow({
-      start: _start,
-      end: _end,
-      amount: _amount.add(escrowedBalances[_address][0].amount)
-    });
+    escrows[id] = Escrow({start: _start, end: _end, balance: amount_});
+    escrowIds[account_].push(id);
 
     emit Locked(_address, _amount);
   }
 
-  function _claimFor(address _address) internal {
-    uint256 claimable = vested[_address];
-    require(claimable > 0, "nothing to claim");
-    if (claimable == escrowedBalances[_address][0].amount) {
-      delete vested[_address];
-      delete escrowedBalances[_address][0];
-    } else {
-      vested[_address] = 0;
-      escrowedBalances[_address][0].amount = escrowedBalances[_address][0]
-      .amount
-      .sub(claimable);
+  /**
+   * @notice Claim rewards of a vault
+   * @param index_ uint256
+   * @dev Uses the vaultId_ at the specified index of userVaults.
+   * @dev This function is used when a user only wants to claim a specific vault or if they decide the gas cost of claimRewards are to high for now.
+   * @dev (lower cost but also lower reward)
+   */
+  function claimReward(uint256 index_) external nonReentrant {
+    bytes32 vaultId_ = userVaults[msg.sender][index_];
+    require(vaults[vaultId_].status == VaultStatus.Open, "vault is not open");
+    require(!vaults[vaultId_].claimed[msg.sender], "already claimed");
+
+    uint256 reward = _claimVaultReward(vaultId_, index_, msg.sender);
+    require(reward > 0, "no rewards");
+
+    POP.safeTransfer(msg.sender, reward);
+
+    emit RewardsClaimed(msg.sender, reward);
+  }
+
+  /**
+   * @notice Claim rewards of a a number of vaults
+   * @param indices_ uint256[]
+   * @dev Uses the vaultIds at the specified indices of userVaults.
+   * @dev This function is used when a user only wants to claim multiple vaults at once (probably most of the time)
+   * @dev The array of indices is limited to 19 as we want to prevent gas overflow of looping through too many vaults
+   */
+  function claimRewards(uint256[] calldata indices_) external nonReentrant {
+    require(indices_.length < 5, "claiming too many vaults");
+    uint256 total;
+
+    for (uint256 i = 0; i < indices_.length; i++) {
+      bytes32 escrowId = escrowIds[msg.sender][indices_[i]];
+      if (
+        escrows[escrowId].start ==  &&
+        !escrows[escrowId].claimed[msg.sender]
+      ) {
+        total = total.add(_claimVaultReward(vaultId_, indices_[i], msg.sender));
+      }
     }
-    POP.safeTransfer(_address, claimable);
-    emit Claimed(_address, claimable);
+    require(total > 0, "no rewards");
+
+    POP.safeTransfer(msg.sender, total);
+
+    emit RewardsClaimed(msg.sender, total);
   }
 
-  function setStaking(IStaking _staking) external onlyOwner {
-    require(Staking != _staking, "Same Staking");
-    Staking = _staking;
-    emit StakingChanged(_staking);
+  /**
+   * @notice Underlying function to calculate the rewards that a user gets and set the vault to claimed
+   * @param vaultId_ Bytes32
+   * @param index_ uint256
+   * @param account_ address
+   * @dev We dont want it to error when a vault is empty for the user as this would terminate the entire loop when used in claimRewards()
+   */
+  function _claimVaultReward(
+    bytes32 vaultId_,
+    uint256 index_,
+    address account_
+  ) internal returns (uint256) {
+    uint256 userShares = vaults[vaultId_].shareBalances[account_];
+    if (userShares > 0) {
+      uint256 reward = vaults[vaultId_].tokenBalance.mul(userShares).div(
+        vaults[vaultId_].shares
+      );
+      vaults[vaultId_].tokenBalance = vaults[vaultId_].tokenBalance.sub(reward);
+      vaults[vaultId_].claimed[account_] = true;
+
+      delete userVaults[account_][index_];
+      return reward;
+    }
+    return 0;
   }
 
-  function updateEscrowDuration(uint256 _escrowDuration) external onlyOwner {
-    escrowDuration = _escrowDuration;
-    emit EscrowDurationChanged(_escrowDuration);
+  /* ========== RESTRICTED FUNCTIONS ========== */
+  /**
+   * @notice Sets the budget of rewards in POP per vault
+   * @param amount uint256 reward amount in POP per vault
+   * @dev When opening a vault this contract must have enough POP to fund the rewardBudget of the new vault
+   */
+  function setRewardsBudget(uint256 amount) external onlyGovernance {
+    require(amount > 0, "must be larger 0");
+    rewardBudget = amount;
+    emit RewardBudgetChanged(amount);
   }
 
-  function updateCliff(uint256 _vestingCliff) external onlyOwner {
-    vestingCliff = _vestingCliff;
-    emit VestingCliffChanged(_vestingCliff);
+  /**
+   * @notice Transfer POP to the contract for vault rewards
+   * @param amount uint256 amount in POP to be used for vault rewards
+   * @dev Sufficient RewardsBalance will be checked when opening a new vault to see if enough POP exist to support the new Vault
+   */
+  function contributeReward(uint256 amount) external {
+    require(amount > 0, "must be larger 0");
+    POP.safeTransferFrom(msg.sender, address(this), amount);
+    rewardBalance = rewardBalance.add(amount);
+    emit RewardBalanceIncreased(msg.sender, amount);
   }
 
   /* ========== MODIFIERS ========== */
 
-  modifier updateVested(address _address) {
-    vested[_address] = vested[_address].add(getVested(_address));
+  /**
+   * @notice Modifier to check if a vault exists
+   * @param vaultId_ Bytes32
+   */
+  modifier vaultExists(bytes32 vaultId_) {
+    require(vaults[vaultId_].endTime > 0, "Uninitialized vault slot");
     _;
   }
 }
