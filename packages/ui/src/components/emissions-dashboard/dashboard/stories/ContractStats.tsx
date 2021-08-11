66287ba01f46e8bcc86934e0f3e8801b2a03256f
mefuru <mehul.mandania@gmail.com>
2021-08-11 20:47:23 +0200

    Add dashboard elements

--- packages/ui/src/components/emissions-dashboard/dashboard/stories/ContractStats.tsx
+++ packages/ui/src/components/emissions-dashboard/dashboard/stories/ContractStats.tsx
@@ -1,35 +1,23 @@
 /* This example requires Tailwind CSS v2.0+ */
-import {
-  CursorClickIcon,
-  MailOpenIcon,
-  UsersIcon,
-} from '@heroicons/react/outline';
+import { CloudIcon, TrendingUpIcon } from '@heroicons/react/outline';
 import { ArrowSmDownIcon, ArrowSmUpIcon } from '@heroicons/react/solid';
 
 const stats = [
   {
     id: 1,
-    name: 'Total Subscribers',
-    stat: '71,897',
-    icon: UsersIcon,
-    change: '122',
+    name: 'CO2 Emissions',
+    stat: '71,897kg',
+    icon: CloudIcon,
+    change: '12.38%',
     changeType: 'increase',
   },
   {
     id: 2,
-    name: 'Avg. Open Rate',
-    stat: '58.16%',
-    icon: MailOpenIcon,
+    name: 'Transactions',
+    stat: '23,123',
+    icon: TrendingUpIcon,
     change: '5.4%',
     changeType: 'increase',
-  },
-  {
-    id: 3,
-    name: 'Avg. Click Rate',
-    stat: '24.57%',
-    icon: CursorClickIcon,
-    change: '3.2%',
-    changeType: 'decrease',
   },
 ];
 
@@ -39,17 +27,24 @@ function classNames(...classes) {
 
 export default function Example() {
   return (
-    <div>
-      <h3 className="text-lg leading-6 font-medium text-gray-900">
-        Last 30 days
-      </h3>
-
+    <div className="pb-4">
       <dl className="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3">
+        <div className="relative bg-white pt-5 px-6 pb-6 sm:pt-6 sm:px-6 overflow-hidden">
+          <p className="my-auto text-2xl font-semibold text-gray-900">
+            Popcorn HYSI Staking Pool
+          </p>
+        </div>
         {stats.map((item) => (
           <div
             key={item.id}
-            className="relative bg-white pt-5 px-4 pb-12 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
+            className="relative bg-white pt-5 px-6 pb-6 sm:pt-6 sm:px-6 shadow rounded-lg overflow-hidden"
           >
+            <dt>
+              <div className="absolute bg-indigo-500 rounded-md p-3">
+                <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
+              </div>
+            </dt>
+
             <dt>
               <div className="absolute bg-indigo-500 rounded-md p-3">
                 <item.icon className="h-6 w-6 text-white" aria-hidden="true" />
@@ -58,7 +53,7 @@ export default function Example() {
                 {item.name}
               </p>
             </dt>
-            <dd className="ml-16 pb-6 flex items-baseline sm:pb-7">
+            <dd className="ml-16  flex items-baseline ">
               <p className="text-2xl font-semibold text-gray-900">
                 {item.stat}
               </p>
@@ -88,17 +83,6 @@ export default function Example() {
                 </span>
                 {item.change}
               </p>
-              <div className="absolute bottom-0 inset-x-0 bg-gray-50 px-4 py-4 sm:px-6">
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    {' '}
-                    View all<span className="sr-only"> {item.name} stats</span>
-                  </a>
-                </div>
-              </div>
             </dd>
           </div>
         ))}
--- packages/ui/src/components/emissions-dashboard/dashboard/stories/PageHeader.tsx
+++ packages/ui/src/components/emissions-dashboard/dashboard/stories/PageHeader.tsx
@@ -4,7 +4,7 @@ export default function Example() {
     <div className="md:flex md:items-center md:justify-between">
       <div className="flex-1 min-w-0">
         <h2 className="text-2xl font-bold leading-7 text-gray-900 sm:text-3xl sm:truncate">
-          Back End Developer
+          Smart Contract Carbon Emissions Dashboard
         </h2>
       </div>
       <div className="mt-4 flex md:mt-0 md:ml-4">
