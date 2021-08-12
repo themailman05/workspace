1eb8254678a4df2010192166ec0edf1e45169437
mefuru <mehul.mandania@gmail.com>
2021-08-12 10:45:50 +0200

    Update PageHeader and add LineChart

--- packages/ui/src/components/emissions-dashboard/dashboard/stories/AddContractButton.tsx
+++ packages/ui/src/components/emissions-dashboard/dashboard/stories/AddContractButton.tsx
@@ -2,7 +2,7 @@ import { PlusIcon as PlusIconOutline } from '@heroicons/react/outline';
 
 export default function Example() {
   return (
-    <div className="justify-self-center flex flex-wrap content-center">
+    <div className="flex flex-wrap content-center">
       <button
         type="button"
         className="inline-flex mr-4 items-center p-2 border border-transparent rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
--- packages/ui/src/components/emissions-dashboard/dashboard/stories/PageHeader.tsx
+++ packages/ui/src/components/emissions-dashboard/dashboard/stories/PageHeader.tsx
@@ -8,17 +8,46 @@ export default function Example() {
         </h2>
       </div>
       <div className="mt-4 flex md:mt-0 md:ml-4">
-        <button
-          type="button"
-          className="inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Edit
-        </button>
+        <div className="mx-2">
+          <label
+            htmlFor="start"
+            className="block text-sm font-medium text-gray-700"
+          >
+            Start
+          </label>
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <input
+              type="date"
+              name="start"
+              id="start"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-10 sm:text-sm border-gray-300 rounded-md"
+              placeholder="05/05/2021"
+            />
+          </div>
+        </div>
+
+        <div>
+          <label
+            htmlFor="end"
+            className="block text-sm font-medium text-gray-700"
+          >
+            End
+          </label>
+          <div className="mt-1 relative rounded-md shadow-sm">
+            <input
+              type="date"
+              name="end"
+              id="end"
+              className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-10 sm:text-sm border-gray-300 rounded-md"
+              placeholder="05/10/2021"
+            />
+          </div>
+        </div>
         <button
           type="button"
           className="ml-3 inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
-          Publish
+          Update
         </button>
       </div>
     </div>
