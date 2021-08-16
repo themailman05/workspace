47dee92b253b17d926e6fb0dcfa38a4bafe70a53
Anthony Martin <anthony@replaycreative.com>
2021-08-16 12:51:05 +0200

    updates id for tooltip

--- packages/ui/src/components/popcorn/Tooltip/index.tsx
+++ packages/ui/src/components/popcorn/Tooltip/index.tsx
@@ -1,22 +1,28 @@
+import React from 'react';
 import { QuestionMarkCircleIcon } from '@heroicons/react/outline';
 import ReactTooltip from 'react-tooltip';
 
 export interface TooltipProps {
   title: string;
   content: string | React.ReactElement;
+  id?: string;
   place?: "bottom" | "left" | "right" | "top";
+  size?: number;
 }
 
-export const Tooltip: React.FC<TooltipProps> = ({ title, content, place }) => {
+export const Tooltip: React.FC<TooltipProps> = ({ title, content, place, id, size }) => {
+  size = size || 4;
+  id = id || "tooltip-" + (new Date().getTime());
+
   return (
     <div className="">
       <QuestionMarkCircleIcon
         data-tip
-        data-for="tooltip"
-        className={`cursor-pointer h-4 w-4`}
+        data-for={id}
+        className={`cursor-pointer h-${size} w-${size}`}
       />
       <ReactTooltip
-        id="tooltip"
+        id={id}
         place={place || "bottom"}
         effect="solid"
         type="light"
