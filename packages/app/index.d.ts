9a3fafc23bea23913888399e3568fa303be85e62
johnmgrimm <jmgtc@protonmail.com>
2021-08-16 09:24:41 +0200

    Add types for *.svg import

--- packages/app/index.d.ts
+++ packages/app/index.d.ts
@@ -1,4 +1,13 @@
 declare global {
   interface Window { ethereum: any }
 }
+
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react';
+  export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
+
+  const src: string;
+  export default src;
+}
+
 window.ethereum = window.ethereum || {};
