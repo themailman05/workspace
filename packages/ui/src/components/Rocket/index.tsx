aa586c7bd1a5929bc17a6f52778f63dd4a3d0b56
johnmgrimm <jmgtc@protonmail.com>
2021-08-17 00:03:51 +0200

    Add more wobble

--- packages/ui/src/components/Rocket/index.tsx
+++ packages/ui/src/components/Rocket/index.tsx
@@ -2,48 +2,43 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { ReactComponent as RocketSvg } from './rocket.svg';
 
+const shakeDiff = 2;
 const rocketAnimation = keyframes`
-from, to { transform: translate3d(-1px, 0, 0) rotate3d(0,0,1,-0.4deg); }
-25% { transform: translate3d(1px, 0, 0) rotate3d(0,0,1,0.4deg); }
-50% { transform: translate3d(-1px, 0, 0) rotate3d(0,0,1,-0.4deg); }
-75% { transform: translate3d(1px, 0, 0) rotate3d(0,0,1,0.4deg); }
+  from, to { transform: translate3d(0, 0, 0); }
+  25% { transform: translate3d(${shakeDiff}px, ${shakeDiff}px, 0); }
+  50% { transform: translate3d(-${shakeDiff}px, -${shakeDiff}px, 0); }
+  60% { transform: translate3d(${shakeDiff}px, ${shakeDiff}px, 0); }
+  80% { transform: translate3d(-${shakeDiff / 2}px, -${shakeDiff / 2}px, 0); }
+  95% { transform: translate3d(${shakeDiff / 2}px, ${shakeDiff / 2}px, 0); }
 `;
 
-const planetAnimation = keyframes`
-from,
-to {
-  transform: translate3d(0, 0, 0);
-}
-50% {
-  transform: translate3d(4px, 0, 0);
-}
-75% {
-  transform: translate3d(2px, 2px, 0) scale3d(1.05,1.05,1);
-}
+const wobble1 = keyframes`
+  from, to { transform: translate3d(0, 0,0) scale3d(0.98,0.98,0.98); }
+  25% { transform: translate3d(-3px, 0,0) scale3d(1,1,1); }
+  50% { transform: translate3d(0, -1px,0) scale3d(1.02,1.02,1.02); }
+  75% { transform: translate3d(3px, 0,0) scale3d(1,1,1); }
 `;
 
-const jupiterAnimation = keyframes`
-from,
-to {
-  transform: translate3d(5px, 0, 0);
-}
-25% {
-  transform: translate3d(0, 0, 0) scale3d(1.05,1.05,1);
-}
-50% {
-  transform: translate3d(-5px, 0, 0);
-}
+const wobble2 = keyframes`
+  from, to { transform: translate3d(0, 0,0) scale3d(0.98,0.98,0.98); }
+  25% { transform: translate3d(2px, -1px,0) scale3d(1,1,1); }
+  50% { transform: translate3d(-2px, 1px,0) scale3d(1.02,1.02,1.02); }
+  75% { transform: translate3d(-2px, 1px,0) scale3d(1,1,1); }
+`;
+
+const wobble3 = keyframes`
+  from, to { transform: translate3d(0, 0,0) scale3d(0.98,0.98,0.98); }
+  25% { transform: translate3d(-2px, 0,0) scale3d(1,1,1); }
+  50% { transform: translate3d(-1px, 1px,0) scale3d(1.02,1.02,1.02); }
+  75% { transform: translate3d(2px, 2px,0) scale3d(1,1,1); }
 `;
 
 const starAnimation = keyframes`
-from,
-to {
-  transform: scale3d(1, 1, 1);
-}
-
-50% {
-  transform: scale3d(1.2, 1.2, 1) rotate3d(0,0,1,-5deg);
-}`;
+  from, to { transform: translate3d(0, 0,0) scale3d(0.98, 0.98, 0.98); }
+  25% { transform: translate3d(2px, -1px,0) scale3d(1,1,1); }
+  50% { transform: translate3d(0, 1px,0) scale3d(1.3, 1.3, 1) rotate3d(0,0,1,-5deg); }
+  75% { transform: translate3d(-2px, 1px,0) scale3d(1,1,1); }
+`;
 
 const StyledRocket = styled(RocketSvg)`
   position: relative;
@@ -53,12 +48,37 @@ const StyledRocket = styled(RocketSvg)`
   * {
     transform-box: fill-box;
     transform-origin: center center;
+    transition-timing-function: ease;
+  }
+
+  #jupiter1,
+  #planet2,
+  #planet5 {
+    animation: ${wobble1} 1s infinite ease;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in;
+  }
+
+  #jupiter2,
+  #planet3,
+  #planet6 {
+    animation: ${wobble2} 1.1s infinite ease;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in;
+  }
+
+  #jupiter3,
+  #planet1,
+  #planet4 {
+    animation: ${wobble3} 1.2s infinite ease;
+    animation-iteration-count: infinite;
+    animation-timing-function: ease-in;
   }
 
   #ship {
     transform-origin: 75% 20%;
     animation: ${rocketAnimation};
-    animation-duration: 1.5s;
+    animation-duration: 2s;
     animation-iteration-count: infinite;
     animation-timing-function: ease-in-out;
   }
@@ -67,7 +87,7 @@ const StyledRocket = styled(RocketSvg)`
   #star3,
   #star4 {
     animation: ${starAnimation};
-    animation-duration: 3s;
+    animation-duration: 2s;
     animation-iteration-count: infinite;
     animation-timing-function: ease-in;
   }
@@ -75,21 +95,10 @@ const StyledRocket = styled(RocketSvg)`
     animation-delay: -0.2s;
   }
   #star3 {
-    animation-delay: -0.4s;
+    animation-delay: -0.3s;
   }
   #star4 {
-    animation-delay: -0.6s;
-  }
-  #planet1,
-  #planet2,
-  #planet3,
-  #planet4,
-  #planet5,
-  #planet6 {
-    animation: ${planetAnimation};
-    animation-duration: 3s;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-in;
+    animation-delay: -0.7s;
   }
   #planet2 {
     animation-delay: -0.2s;
@@ -106,20 +115,11 @@ const StyledRocket = styled(RocketSvg)`
   #planet6 {
     animation-delay: -0.6s;
   }
-
-  #jupiter1,
-  #jupiter2,
-  #jupiter3 {
-    animation: ${jupiterAnimation};
-    animation-duration: 5s;
-    animation-iteration-count: infinite;
-    animation-timing-function: ease-in;
-  }
   #jupiter2 {
-    animation-delay: -1s;
+    animation-delay: -0.3s;
   }
   #jupiter3 {
-    animation-delay: -2s;
+    animation-delay: -0.4s;
   }
 `;
 
