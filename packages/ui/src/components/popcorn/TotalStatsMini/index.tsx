676048de929b1cd2282ee49405de2e551eeeabf3
mefuru <mehul.mandania@gmail.com>
2021-08-17 14:26:15 +0200

    Create dashboard page using mini total stats

--- packages/ui/src/components/popcorn/TotalStatsMini/index.tsx
+++ packages/ui/src/components/popcorn/TotalStatsMini/index.tsx
@@ -1,4 +1,4 @@
-import { SVGProps } from 'react';
+import React, { SVGProps } from 'react';
 import { MiniStatCard } from '../MiniStatCard';
 import { EmissionsAreaChart } from '../recharts/AreaChart';
 import { getDummyEmissionData } from '../recharts/dummyEmissionsData';
@@ -20,13 +20,19 @@ export const TotalStatsMini: React.FC<TotalStatsProps> = ({
   emissionSummaryStats,
 }) => {
   return (
-    <div className="pb-8 bg-gray-50">
-      <h3 className="text-lg leading-6 font-medium text-gray-900 mt-16">
-        Totals between 12/04/2021 and 30/05/2021
-      </h3>
-      <div className="grid sm:grid-cols-2 md:grid-cols-5  gap-4">
+    <div className="pb-8 my-8 bg-gray-50">
+      <div className="grid sm:grid-cols-2 md:grid-cols-5 gap-4">
         <div className="col-span-3">
-          <dl className="mt-5 grid grid-cols-3 gap-4 sm:grid-cols-2 lg:grid-cols-4 mx-4">
+          <dl className="mt-5 grid grid-cols-2 gap-4 sm:grid-cols-2 lg:grid-cols-4 mx-4 ">
+            <div className="mt-2">
+              <dt>
+                <p className="text-lg leading-6 font-medium text-gray-900">
+                  Total Stats
+                </p>
+              </dt>
+              <dd className=" text-base text-gray-500">19 Aug 2021 - 09:12</dd>
+            </div>
+
             {emissionSummaryStats.map((item) => (
               <MiniStatCard item={item} />
             ))}
---
+++ packages/ui/src/components/popcorn/emissions-dashboard/dashboard/stories/mini.stories.tsx
@@ -0,0 +1,70 @@
+import {
+  CloudIcon,
+  CursorClickIcon,
+  TrendingUpIcon,
+} from '@heroicons/react/outline';
+import { Meta, Story } from '@storybook/react/types-6-0';
+import { AddContractButton } from '../../../AddContractButton';
+import { ContractStats } from '../../../ContractStats';
+import { Divider } from '../../../Divider';
+import { PageHeader } from '../../../PageHeader';
+import { TotalStatsMini } from '../../../TotalStatsMini';
+
+const EmissionsDashboardPage = () => {
+  return (
+    <div className="bg-gray-50">
+      <PageHeader />
+      <TotalStatsMini
+        emissionSummaryStats={[
+          {
+            id: 1,
+            name: 'co2Emissions',
+            stat: '71kg',
+            icon: CloudIcon,
+            change: '12.38%',
+            changeType: 'increase',
+          },
+          {
+            id: 2,
+            name: 'Transactions',
+            stat: '23',
+            icon: TrendingUpIcon,
+            change: '5.4%',
+            changeType: 'increase',
+          },
+          {
+            id: 3,
+            name: 'Average Gas Price',
+            stat: '45',
+            icon: CursorClickIcon,
+            change: '3.2%',
+            changeType: 'decrease',
+          },
+        ]}
+      />
+      <Divider />
+      <ContractStats />
+      <ContractStats />
+      <ContractStats />
+      <ContractStats />
+      <AddContractButton />
+    </div>
+  );
+};
+
+export default {
+  title: 'Emissions Dashboard / Dashboard / Mini',
+  component: EmissionsDashboardPage,
+  decorators: [
+    (Story) => (
+      <div className="flex flex-row justify-center">
+        <Story></Story>
+      </div>
+    ),
+  ],
+} as Meta;
+
+const Template: Story = (args) => <EmissionsDashboardPage {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {};
