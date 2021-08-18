956c50bd7278f5d3668565c295dcdef5d4702090
Anthony Martin <anthony@replaycreative.com>
2021-08-18 14:24:52 +0200

    updates favicon and adds open graph tags

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -54,6 +54,24 @@ export default function MyApp(props) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
+
+        <meta name="description" content="Earn high yield on your crypto assets while creating real world impact. Your fees go directly towards non-profits" />
+
+        {/*  Facebook Meta Tags */}
+        <meta property="og:url" content="https://popcorn.network/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Popcorn - DeFi for the People" />
+        <meta property="og:description" content="Earn high yield on your crypto assets while creating real world impact. Your fees go directly towards non-profits" />
+        <meta property="og:image" content="https://popcorn.network/images/popcorn_network_rocket.png" />
+
+        {/* Twitter Meta Tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="popcorn.network" />
+        <meta property="twitter:url" content="https://popcorn.network/" />
+        <meta name="twitter:title" content="Popcorn - DeFi for the People" />
+        <meta name="twitter:description" content="Earn high yield on your crypto assets while creating real world impact. Your fees go directly towards non-profits" />
+        <meta name="twitter:image" content="https://popcorn.network/images/popcorn_network_rocket.png" />
+
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
