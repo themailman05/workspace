d766ca8258c0401d1a00d7ecdb978e929d5bf824
Liam Sargent <themailman05@gmail.com>
2021-08-18 11:29:30 -0500

    Add support for SVG load

--- packages/ui/src/index.d.ts
+++ packages/ui/src/index.d.ts
@@ -1,7 +1,8 @@
 declare module 'tabler-react';
-declare module "*.svg" {
-  const content: any;
-  export default content;
-  export { ReactComponent };
-}
+declare module '*.svg' {
+  import { FC, SVGProps } from 'react';
+  export const ReactComponent: FC<SVGProps<SVGSVGElement>>;
 
+  const src: string;
+  export default src;
+}
