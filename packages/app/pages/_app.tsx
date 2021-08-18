28bf36b07941566daaab693ea1ddd3dcc273c72e
Anthony Martin <anthony@replaycreative.com>
2021-08-18 14:43:34 +0200

    updates language

--- packages/app/pages/_app.tsx
+++ packages/app/pages/_app.tsx
@@ -55,13 +55,13 @@ export default function MyApp(props) {
           content="minimum-scale=1, initial-scale=1, width=device-width"
         />
 
-        <meta name="description" content="Earn high yield on your crypto assets while creating real world impact. Your fees go directly towards non-profits" />
+        <meta name="description" content="Earn high yield on your cryptoassets while creating real world impact. Our fees go directly towards non-profits" />
 
         {/*  Facebook Meta Tags */}
         <meta property="og:url" content="https://popcorn.network/" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Popcorn - DeFi for the People" />
-        <meta property="og:description" content="Earn high yield on your crypto assets while creating real world impact. Your fees go directly towards non-profits of your choosing." />
+        <meta property="og:description" content="Earn high yield on your cryptoassets while creating real world impact. Our fees go directly towards non-profits of your choosing." />
         <meta property="og:image" content="https://popcorn.network/images/popcorn_network_rocket.png" />
 
         {/* Twitter Meta Tags */}
@@ -69,7 +69,7 @@ export default function MyApp(props) {
         <meta property="twitter:domain" content="popcorn.network" />
         <meta property="twitter:url" content="https://popcorn.network/" />
         <meta name="twitter:title" content="Popcorn - DeFi for the People" />
-        <meta name="twitter:description" content="Earn high yield on your crypto assets while creating real world impact. Your fees go directly towards non-profits of your choosing." />
+        <meta name="twitter:description" content="Earn high yield on your cryptoassets while creating real world impact. Our fees go directly towards non-profits of your choosing." />
         <meta name="twitter:image" content="https://popcorn.network/images/popcorn_network_rocket.png" />
 
         <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
